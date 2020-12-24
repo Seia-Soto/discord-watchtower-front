6083407461e58e2e3332b7e1f8b732465058ff97
Seia-Soto <seia@outlook.kr>
2020-12-24 11:07:44 +0900

    Install snowpack plugins

---
+++ snowpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_'
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    [
+      'snowpack-plugin-import-map',
+      {
+        imports: {
+          '*': true
+        }
+      }
+    ]
+  ]
+}
